bab8f83612e1e1a4e751d8028b8269b52d757f4a
Lauro Ribeiro <lauroribeirola@hotmail.com>
2020-01-06 17:31:09 -0300

    Tudo funcionando

--- script.js
+++ script.js
@@ -7,21 +7,29 @@ var afazeres = [
     'Fazer um Web Service',
     'Fazer trabalho de PAA',
     'Estudar Física'
-    ];
+];
 
-function renderAfazeres(){
+function renderAfazeres() {
     listElement.innerHTML = '';
-    for(afazer of afazeres){
+    for (afazer of afazeres) {
         var afazerElement = document.createElement('li');
         var afazerText = document.createTextNode(afazer);
+        var linkElement = document.createElement('a');
+        var textElement = document.createTextNode(' Excluir');
+        var pos = afazeres.indexOf(afazer);
+        linkElement.setAttribute('href', '#');
+        linkElement.setAttribute('onclick', 'removerAfazeres(' + pos + ')');
+        linkElement.appendChild(textElement);
         afazerElement.appendChild(afazerText);
+        afazerElement.appendChild(linkElement);
         listElement.appendChild(afazerElement);
+
     }
 }
 renderAfazeres();
 
-function addAfazeres(){
-    if(inputElement.value != ''){
+function addAfazeres() {
+    if (inputElement.value != '') {
 
         afazeres.push(inputElement.value);
         inputElement.value = '';
@@ -29,4 +37,10 @@ function addAfazeres(){
     }
 }
 
+function removerAfazeres(pos) {
+    afazeres.splice(pos, 1);    //splice vai na posição do array e retira a quantiadade de elementos indicada
+    renderAfazeres();
+}
+
 btnElement.onclick = addAfazeres;
+
